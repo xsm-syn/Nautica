731f61fa2290bc0a90249c98cd0bdfccb63704d8
Muhammad Zikri <83739556+xsm-syn@users.noreply.github.com>
2025-01-03 07:51:25 +0700

    Update proxyip.ts

--- helper/proxyip.ts
+++ helper/proxyip.ts
@@ -23,7 +23,7 @@ interface ProxyTestResult {
 
 const KV_PAIR_PROXY_FILE = "./kvProxyList.json";
 const RAW_PROXY_LIST_FILE = "./rawProxyList.txt";
-const PROXY_LIST_FILE = "./proxy.txt";
+const PROXY_LIST_FILE = "./proxyList.txt";
 const IP_RESOLVER_DOMAIN = "https://id1.foolvpn.me/api/v1/check";
 const CONCURRENCY = 99;
 
